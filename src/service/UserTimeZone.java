6f4d010f310efb17b1e6229c10bc712277797ec1
Bene Lorant <lbene@pitechplus.com>
2014-10-21 15:36:39 +0300

    XML parse of timezone result.

--- src/service/UserTimeZone.java
+++ src/service/UserTimeZone.java
@@ -1,14 +1,12 @@
 package service;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 
 /**
  * Created by lbene on 20.10.2014.
@@ -18,9 +16,10 @@ import java.net.URL;
 public class UserTimeZone implements Serializable{
 
     private static final long serialVersionUID = 1L;
-    private static final String serviceUrl = "http://localhost:8080/TimeZoneService_war_exploded/TimeZone";
+    private static final String serviceUrl = "http://localhost:8080/TimeZone_war_exploded/TimeZone";
 
     public String getTimeZone(int coordinateX, int coordinateY) {
+        String result = "Could not retrieve timezone.";
         try {
             URL timeZoneServlet = new URL(serviceUrl+"?long="+ coordinateX + "&lat=" + coordinateY);
             HttpURLConnection servletConnection = (HttpURLConnection) timeZoneServlet.openConnection();
@@ -34,12 +33,13 @@ public class UserTimeZone implements Serializable{
                 str += (char) reader.read();
             }
 
-            return str;
+            result = str.split("<time_zone_id>")[1].split("</time_zone_id>")[0];
         } catch (MalformedURLException ee) {
             ee.printStackTrace();
         } catch (IOException ee) {
             ee.printStackTrace();
         }
-        return "Timezone could not be retrieved.";
+
+        return result;
     }
 }
